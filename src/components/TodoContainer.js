91bd1c7050e62bd03eb98a4b342384b246e385dd
Joel-100 <joelkugonza@gmail.com>
2022-11-01 11:36:01 +0300

    React part 1

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,33 @@
+import TodosList from "./TodosList"
+import React from "react"
+import Header from "./Header"
+class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        complete: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <TodosList todos={this.state.todos} />
+      </div>
+    );
+  }
+}
+export default TodoContainer
